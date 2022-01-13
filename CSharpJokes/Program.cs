9ad6a12cb7fccc56b87617b15afb2ea522aaf5a5
Alexis <alexisricher@hotmail.com>
2022-01-12 21:37:03 -0500

    RageAgainstTheCommit

---
+++ CSharpJokes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpJokes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new RageAgainstTheMachine();
+        }
+    }
+}
---
+++ CSharpJokes/RageAgainstTheMachine.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace CSharpJokes
+{
+    public class RageAgainstTheMachine
+    {
+        const string ThanksToThatGuyForTheASCIIArt = "https://asciiart.website/index.php?art=music/musical%20instruments";
+        public RageAgainstTheMachine()
+        {
+            foreach (var Killing in The(nameof(RageAgainstTheMachine)))
+            {
+                System.Console.WriteLine
+                (
+                    @"     .._       o' o'                     (())))     _    o' o'                 " + "\n" +
+                    @"    //\\\    |  __                      )) _ _))  ,' ; |  __                   " + "\n" +
+                    @"   ((-.-\)  o' |--|  ,;::::;.          (C    )   / /^ o' |--|                  " + "\n" +
+                    @"  _))'='(\-.  o' o' ,:;;;;;::.         )\   -'( / /     o' o'                  " + "\n" +
+                    @" (          \       :' o o `::       ,-)()  /_.')/                 .           " + "\n" +
+                    @" | | .)(. |\ \      (  (_    )      /  (  `'  /\_)    .:izf:,_  .  |           " + "\n" +
+                    @" | | _   _| \ \     :| ,==. |:     /  ,   _  / 1  \ .:q568Glip-, \ |           " + "\n" +
+                    @" \ \/ '-' (__\_\____::\`--'/::    /  /   / \/ /|\  \-38'^'^`8k='  \L,          " + "\n" +
+                    @"  \__\\[][]____(_\_|::,`--',::   /  /   /__/ <(  \  \8) o o 18-'_ ( /          " + "\n" +
+                    @"   :\o*.-.(     '-,':   _    :`.|  L----' _)/ ))-..__)(  J  498:- /]           " + "\n" +
+                    @"   :   [   \     |     |=|   '  |\_____|,/.' //.   -38, 7~ P88;-'/ /           " + "\n" +
+                    @"   :  | \   \    |  |  |_|   |  |    ||  :: (( :   :  ,`'''`-._,' /            " + "\n" +
+                    @"   :  |  \   \   ;  |   |    |  |    \ \_::_)) |  :  ,     ,_    /             " + "\n" +
+                    @"   :( |   /  )) /  /|   |    |  |    |    [    |   \_\      _;--==--._         " + "\n" +
+                    @"   :  |  /  /  /  / |   |    |  |    |    Y    |CJR (_\____:_        _:        " + "\n" +
+                    @"   :  | /  / _/  /  \   |lf  |  |  CJ|mk  |    | ,--==--.  |_`--==--'_|        " + "\n" +
+                    @"                                                             `--==--'          "
+                );
+            }
+        }
+
+        public List<int> The(string bestBandInTheWorld)
+        {
+            const int onlyOneBandYaKnow = 1;
+            return new List<int>() { onlyOneBandYaKnow };
+        }
+    }
+}
